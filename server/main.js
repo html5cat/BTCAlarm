a7480ef640f58cb30f6b50ab98046f4e804343e2
Yuriy Dybskiy <yuriy@dybskiy.com>
2013-11-22 10:55:43 -0800

    Add basic price change tracking

--- server/main.js
+++ server/main.js
@@ -2,36 +2,45 @@ Meteor.startup(function () {
   twilio = Twilio(Meteor.settings.Twilio.sid,
                   Meteor.settings.Twilio.auth);
 
-  // twilio.listSms({
-  //   // from:'+16042272151'
-  // }, function (err, responseData) {
-  //   responseData.smsMessages.forEach(function(message) {
-  //       console.log('Msg sent on: '+ message.dateCreated.toLocaleDateString() + ' From: ' + message.from + ' To: ' + message.to + ' Status: ' + message.status);
-  //       console.log(message.body);
-  //   });
-  // });
-
   firebase = new Firebase('https://publicdata-bitcoin.firebaseio.com/');
 
   firebase.child("last").on("value", showPrice);
   // firebase.child("ask").on("value", showPrice);
 
+  var previous,
+      delta = 5;
+
   function showPrice(snapshot) {
-    console.log(snapshot.name() + ": " + snapshot.val());
+    if (previous === undefined) {
+      previous = snapshot;
+    }
+
+    if (Math.abs(previous.val() - snapshot.val() >= delta) {
+      sendSMS('BTC is now ' + snapshot.val());
+      previous = snapshot;
+
+    }
+
+    // console.log(snapshot.name() + ": " + snapshot.val());
   }
 
-  // twilio.sendSms({
-  //   to:'+16045052916',
-  //   from: '+16042272151',
-  //   body: 'BTC is on fire!'
-  // }, function(err, responseData) { //this function is executed when a response is received from Twilio
-  //   if (!err) { // "err" is an error received during the request, if any
-  //     // "responseData" is a JavaScript object containing data received from Twilio.
-  //     // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-  //     // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-  //     console.log(responseData.from); // outputs "+14506667788"
-  //     console.log(responseData.body); // outputs "word to your mother."
-  //   }
-  // });
+  function sendSMS(msg) {
+    console.log('SMS: ' + msg);
+
+    // twilio.sendSms({
+    //   to: '+16045052916',
+    //   from: '+16042272151',
+    //   body: 'BTC is on fire!'
+    // }, function(err, responseData) { //this function is executed when a response is received from Twilio
+    //   if (!err) { // "err" is an error received during the request, if any
+    //     // "responseData" is a JavaScript object containing data received from Twilio.
+    //     // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+    //     // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+    //     console.log(responseData.from); // outputs "+14506667788"
+    //     console.log(responseData.body); // outputs "word to your mother."
+    //   }
+    // });
+  }
+
 });
 
